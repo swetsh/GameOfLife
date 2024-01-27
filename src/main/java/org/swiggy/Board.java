a7e337236121fe53cfb2d69d335bf1ae295a948b
swet.shreet_ftc <shreet.swet_ftc@external.swiggy.in>
2024-01-27 19:08:12 +0530

    Implemented board class with toString

--- src/main/java/org/swiggy/Board.java
+++ src/main/java/org/swiggy/Board.java
@@ -3,19 +3,35 @@ package org.swiggy;
 public class Board {
     private Cell[][] cellGrid;
 
-    private int rows;
-    private int columns;
+    private final int rows;
+    private final int columns;
 
-    public Board(int rows, int columns) {
+    public Board(int rows, int columns, double seed) {
+        if (rows <= 0 || columns <= 0) throw new RuntimeException();
+        if (seed < 0 || seed > 1) throw new RuntimeException();
+
         this.rows = rows;
         this.columns = columns;
         cellGrid = new Cell[rows][columns];
 
         for (int i=0; i<this.rows; i++) {
             for (int j=0; j<this.columns; j++) {
-                cellGrid[i][j] = new Cell(Cell.State.Dead);
+                double randomValue = Math.random();
+                Cell.State state = (randomValue < seed) ? Cell.State.Alive : Cell.State.Dead;
+                cellGrid[i][j] = new Cell(state);
             }
         }
     }
 
+    @Override
+    public String toString() {
+        StringBuilder board= new StringBuilder();
+        for(int i=0; i<this.rows; i++) {
+            for(int j=0; j<this.columns; j++) {
+                board.append(cellGrid[i][j].toString());
+            }
+            board.append("\n");
+        }
+        return board.toString();
+    }
 }
--- src/test/java/org/swiggy/BoardTest.java
+++ src/test/java/org/swiggy/BoardTest.java
@@ -5,9 +5,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-    @Test
-    public void TestBoardCreatedWithoutException() {
-        Board board = new Board(0, 0);
 
+    @Test
+    public void TestBoardWithNegativeDimensionExpectedRuntime() {
+        assertThrows(RuntimeException.class, () -> new Board(-2, -2, 0));
+    }
+
+    @Test
+    public void TestBoardWithZeroDimensionExpectedRuntime() {
+        assertThrows(RuntimeException.class, () -> new Board(0, 2, 0));
+    }
+
+    @Test
+    public void TestBoardWithNegativeSeedExpectedRuntime() {
+        assertThrows(RuntimeException.class, () -> new Board(2, 2, -0.1));
+    }
+
+    @Test
+    public void TestBoardWithSeedGreaterThan1ExpectedRuntime() {
+        assertThrows(RuntimeException.class, () -> new Board(2, 2, -1.1));
+    }
+
+    @Test
+    public void Test2x2BoardWithAllDeadCell() {
+        Board board = new Board(2, 2, 0); //Zero seed value represent all dead cell
+
+        String expectedBoard = "--\n--\n";
+
+        assertEquals(expectedBoard, board.toString());
+    }
+
+    @Test
+    public void Test2x2BoardWithAllAliceCell() {
+        Board board = new Board(2, 2, 1); //One seed value represent all alive cell
+
+        String expectedBoard = "**\n**\n";
+
+        assertEquals(expectedBoard, board.toString());
     }
 }
+
